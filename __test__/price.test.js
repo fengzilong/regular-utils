ee999ca112dd568c82f79d004daa2405fcb2dd29
mo <feng_zilong@163.com>
2016-11-01 00:08:26 +0800

    Added number test for price

--- __test__/price.test.js
+++ __test__/price.test.js
@@ -2,8 +2,9 @@ import price from '../src/filters/price';
 
 test( 'price should work', () => {
 	expect( price( '12345678' ) ).toBe( '12,345,678' );
+	expect( price( '123456' ) ).toBe( '123,456' );
 } );
 
-test( 'price should work 2', () => {
-	expect( price( '123456' ) ).toBe( '123,456' );
+test( 'price should work for number', () => {
+	expect( price( 12345678 ) ).toBe( '12,345,678' );
 } );
